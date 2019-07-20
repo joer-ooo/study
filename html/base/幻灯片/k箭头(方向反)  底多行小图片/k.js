4c86fe3da65295caf02803787e8b08b3ce706121
jiao-ooo <1311622542@qq.com>
2019-07-20 11:05:59 +0800

    0

---
+++ html/base/幻灯片/k箭头(方向反)  底多行小图片/k.js
@@ -0,0 +1,20 @@
+// Can also be used with $(document).ready()
+$(window).load(function(){
+    $('#f1').flexslider({
+      animation: "slide",
+      rtl: true,
+      start: function(slider){
+        $('body').removeClass('loading');
+      }
+    });
+   $('#f2').flexslider({
+      animation: "slide",
+      animationLoop: false,
+      itemWidth: 210,
+      itemMargin: 5,
+      pausePlay: true,
+      mousewheel: true,
+      rtl: true,
+      asNavFor:'.flexslider'
+    });
+  });
